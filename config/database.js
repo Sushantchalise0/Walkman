cd67cd65e86c27b455e4cf581b6086f76232b85d
unknown <sushantchlaise0>
2020-12-16 16:02:58 +0545

    New database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
     //mongoDbUrl: 'mongodb://sandeep55:sandeep11@ds161074.mlab.com:61074/walkmancms'
-  mongoDbUrl: 'mongodb://sushant:sushant12@ds063879.mlab.com:63879/walkmancms'
-  // mongoDbUrl: 'mongodb://localhost:27017/walkmancms'
+ // mongoDbUrl: 'mongodb://sushant:sushant12@ds063879.mlab.com:63879/walkmancms'
+   //mongoDbUrl: 'mongodb://localhost:27017/walkmancms'
+   mongoDbUrl: 'mongodb+srv://sushant:sushant12@cluster0.7v9c2.mongodb.net/walkman?retryWrites=true&w=majority'
 };
